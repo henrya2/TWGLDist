ae29949e47d14d02ac4698c5ec4721b83cdcb6fe
Henry Read <henrya2@126.com>
2018-03-30 12:13:32 +0800

    commits to ......

---
+++ resources/js/index.js
@@ -0,0 +1,18 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function() {
+  Array.prototype.forEach.call(document.querySelectorAll('pre>code'), function(section) {
+    // Unwrap
+    const parent = section.parentElement;
+    while (section.firstChild) {
+      const child = section.firstChild;
+      section.removeChild(child);
+      parent.appendChild(child);
+    }
+    parent.removeChild(section);
+    // Add class
+    parent.className = "prettyprint showlinemods";
+  });
+  prettyPrint();
+});
+
